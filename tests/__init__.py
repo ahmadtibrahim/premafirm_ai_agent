a2860bb33cbfcbdb2bd6de49948122482141d944
root <root@mail.premafirm.com>
2026-03-02 00:29:38 -0500

    Remove pycache from repository

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import test_run_planner_service
-from . import test_crm_lead_product_assignment
-from . import test_crm_load_number_and_sales_order_lines
-from . import test_crm_lead_pricing_regression
